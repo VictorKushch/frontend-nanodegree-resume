7ac909ef8754cf6870876a4b15bbd4ef311214a2
victor kushch <victor@ku-64.com>
2016-07-13 22:41:13 -0400

    BIO + WORK

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,104 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+*/
+var test = {};
+
+var bio = {
+        name : "Victor Kushch",
+        role : "Front End Developer",
+        email : "victor@ku-64.com",
+        phone : "617-678-7490",
+        picture_url : "https://www.dropbox.com/s/lgvviu9qytpxw0j/2c5537d.jpg?dl=0",
+        welcome_message : "Ready to change your online business outlook",
+        skills : ["JS", "JSON", "Angular", "Python", "HTML", "CSS", "Boottstrap"]
+    };
+var education = {
+        "shools" : [
+            {
+                "name" : "MTI",
+                "city" : "Mogilev, BY",
+                "degree" : "Master",
+                "major" : ["Processing, Automation"]
+            },
+            {
+                "name" : "Udacity Nanodegree",
+                "city" : "Boston",
+                "degree" : "Nanodegree",
+                "major" : ["Frontend"]
+            }
+        ]
+
+
+    }
+var work = {
+    "work_place" : [
+        {
+          "company" : "Ku64",
+          "title" : "developer",
+          "from" : "09/01/2014",
+          "to" : "current",
+          "experience" : "Developing Mmech, variety front end and back end projcts",
+          "scills" : ["JavaScript", "JSON", "Angular", "HTML", "CSS", "Python"]
+        },
+        {
+          "company" : "Empow Studio",
+          "title" : "developer",
+          "from" : "05/01/2016",
+          "to" : "current",
+          "experience" : "Developing student app, corporate WP site",
+          "scills" : ["JavaScript", "JSON", "Angular", "HTML", "CSS", "Python"]
+        },
+
+        {
+          "company" : "AGST Tax",
+          "title" : "developer",
+          "from" : "09/01/2013",
+          "to" : "09/01/2014",
+          "experience" : "Range of scripts for tax oreparation company for batch processing",
+          "scills" : ["JavaScript", "JSON", "Angular", "HTML", "CSS", "Python"]
+        },
+
+        {
+          "company" : "Biokon",
+          "title" : "developer",
+          "from" : "01/01/2006",
+          "to" : "06/01/2016",
+          "experience" : "Front end developer in web dewelopment agency",
+          "scills" : ["JavaScript", "JSON", "Angular", "HTML", "CSS", "Python", "Photoshop", "Indesign"]
+        }
+    ]
+}
+
+var projects = {
+    "project" : [
+        {
+          "title" : "Empow Studio App",
+          "dates" : "2016",
+          "technology" : ["Html", "Css", "JS", "Bootstrap"],
+          "images" : ["http://image.jpg", "http://image2.jpg"]
+        },
+
+        {
+          "name" : "Casio gift reseller",
+          "dates" : "2016",
+          "technology" : ["Html", "Css"],
+          "images" : ["http://image3.jpg", "http://image4.jpg"]
+        },
+
+        {
+          "name" : "GA100 Casio Landing page",
+          "dates" : "2016",
+          "technology" : ["Html", "Css"],
+          "images" : ["http://image4.jpg", "http://image5.jpg"]
+        },
+
+        {
+          "name" : "Casio gift reseller",
+          "dates" : "2016",
+          "technology" : ["Html", "Css"],
+          "images" : ["http://image6.jpg", "http://image7.jpg"]
+        },
+
+
+    ]
+}
