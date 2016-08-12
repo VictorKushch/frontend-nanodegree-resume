b8b00b958c4acfea566ad0310282741ec8e40151
victor kushch <victor@ku-64.com>
2016-08-11 23:35:09 -0400

    1st fix JSON object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,24 +9,24 @@ var bio = {
 		email: "victor@ku-64.com",
 		github: "https://github.com/VictorKushch",
 		linkedin: "https://www.linkedin.com/in/viktar-kushch-4491a483",
-		current_location: "Boston"
+		location: "Boston"
 	},
 	biopic: "https://media.licdn.com/mpr/mpr/shrink_200_200/p/3/005/059/0f2/2c5537d.jpg",
-	welcome_message: "Ready to change your online business outlook",
+	welcomeMessage: "Ready to change your online business outlook",
 	skills: ["JS", "Angular", "HTML", "CSS", "Boottstrap"]
 };
 var education = {
 	"schools": [{
 			"name": "Udacity Nanodegree",
-			"city": "Boston",
+			"location": "Boston",
 			"degree": "Nanodegree",
-			"major": ["Frontend"],
+			"majors": ["Frontend"],
 			"dates": "2016"
 		}, {
 			"name": "MTI",
-			"city": "Mogilev",
+			"location": "Mogilev",
 			"degree": "Master",
-			"major": ["Processing, Automation"],
+			"majors": ["Processing, Automation"],
 			"dates": "1997 - 2002"
 		}
 
@@ -40,43 +40,39 @@ var education = {
 };
 var work = {
 	"work_place": [{
-			"company": "Ku64",
+			"employer": "Ku64",
 			"title": "developer",
-			"from": "09/01/2014",
-			"to": "current",
-			"experience": "Developing marketmech.com, variety front end and back end projcts",
+			"dates": "09/01/2014 to current",
+			"description": "Developing marketmech.com, variety front end and back end projcts",
 			"location": "Auburndale, MA"
 		}, {
-			"company": "Empow Studio",
+			"employer": "Empow Studio",
 			"title": "developer",
-			"from": "05/01/2016",
-			"to": "current",
-			"experience": "Developing student app, corporate WP site",
+			"dates": "05/01/2016 to current",
+			"description": "Developing student app, corporate WP site",
 			"location": "Lexington, MA"
 		},
 
 		{
-			"company": "AGST Tax",
+			"employer": "AGST Tax",
 			"title": "developer",
-			"from": "09/01/2013",
-			"to": "09/01/2014",
-			"experience": "Range of scripts for tax oreparation company for batch processing",
+			"dates": "09/01/2013 to 09/01/2014",
+			"description": "Range of scripts for tax oreparation company for batch processing",
 			"location": "Newton, MA"
 		},
 
 		{
-			"company": "Biokon",
+			"employer": "Biokon",
 			"title": "developer",
-			"from": "01/01/2006",
-			"to": "06/01/2016",
-			"experience": "Developer in web dewelopment agency",
+			"dates": "01/01/2006 to 06/01/2016",
+			"description": "Developer in web dewelopment agency",
 			"location": "St. Petersburg"
 		}
 	]
 };
 
-var projects = {
-	"project": [{
+var portfolio = {
+	"projects": [{
 			"title": "Empow Studio App",
 			"dates": "2016",
 			"description": "JS and JSON based online App using Large Google Drive aasets in education area",
@@ -106,7 +102,7 @@ bio.display = function() {
 	var FormatedPictureBlock = HTMLbioPic.replace("%data%", bio.biopic);
 	var FormattedName = HTMLheaderName.replace("%data%", intName(bio.name));
 	var FormattedRole = HTMLheaderRole.replace("%data%", bio.role);
-	var FormattedIntro = HTMLwelcomeMsg.replace("%data%", bio.welcome_message);
+	var FormattedIntro = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 	$("#header").prepend(FormattedName + FormattedRole + FormattedIntro);
 	$("#header").append(FormatedPictureBlock);
 
@@ -114,7 +110,7 @@ bio.display = function() {
 	var FormattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
 	var FormatedLinkedIn = HTMLlinkedin.replace("%data%", bio.contacts.linkedin);
 	var Formatedgithub = HTMLgithub.replace("%data%", bio.contacts.github);
-	var FormatedLocation = HTMLlocation.replace("%data%", bio.contacts.current_location);
+	var FormatedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
 
 	var id_strings = ['#topContacts', '#footerContacts'];
 	for (var item = 0; item < id_strings.length; item++) {
@@ -137,12 +133,12 @@ work.display = function() {
 	if (work.work_place.length > 0) {
 		for (var item in work.work_place) {
 			$("#workExperience").append(HTMLworkStart);
-			var FormatedEmployer = HTMLworkEmployer.replace("%data%", work.work_place[item].company);
+			var FormatedEmployer = HTMLworkEmployer.replace("%data%", work.work_place[item].employer);
 			var FormatedTitle = HTMLworkTitle.replace("%data%", work.work_place[item].title);
-			var from_to = work.work_place[item].from + " - " + work.work_place[item].to;
+			var from_to = work.work_place[item].dates;
 			var FormatedFromTo = HTMLworkDates.replace("%data%", from_to);
 			var FormatedLocation = HTMLworkLocation.replace("%data%", work.work_place[item].location);
-			var FormatedDescription = HTMLworkDescription.replace("%data%", work.work_place[item].experience);
+			var FormatedDescription = HTMLworkDescription.replace("%data%", work.work_place[item].description);
 			$(".work-entry:last").append(FormatedEmployer + FormatedTitle + FormatedFromTo + FormatedLocation + FormatedDescription);
 
 		}
@@ -152,19 +148,19 @@ work.display = function() {
 work.display();
 
 //Projects
-projects.display = function() {
-	if (projects.project.length > 0) {
+portfolio.display = function() {
+	if (portfolio.projects.length > 0) {
 		$("#projects").append(HTMLprojectStart);
-		for (var item in projects.project) {
+		for (var item in portfolio.projects) {
 
-			var FormatedProjectTitle = HTMLprojectTitle.replace("%data%", projects.project[item].title);
+			var FormatedProjectTitle = HTMLprojectTitle.replace("%data%", portfolio.projects[item].title);
 			$(".project-entry:last").append(FormatedProjectTitle);
-			var FormatedProjectDates = HTMLprojectDates.replace("%data%", projects.project[item].dates);
+			var FormatedProjectDates = HTMLprojectDates.replace("%data%", portfolio.projects[item].dates);
 			$(".project-entry:last").append(FormatedProjectDates);
-			var FormatedProjectDescription = HTMLprojectDescription.replace("%data%", projects.project[item].description);
+			var FormatedprojectsDescription = HTMLprojectDescription.replace("%data%", portfolio.projects[item].description);
 			$(".project-entry:last").append(FormatedProjectDescription);
-			for (var image in projects.project[item].images) {
-				var FormatedProjectImage = HTMLprojectImage.replace("%data%", projects.project[item].images[image]);
+			for (var image in portfolio.projects[item].images) {
+				var FormatedProjectImage = HTMLprojectImage.replace("%data%", portfolio.projects[item].images[image]);
 				$(".project-entry:last").append(FormatedProjectImage);
 			}
 			// $("#projects").append(FormatedProjectTitle + FormatedProjectDates + FormatedProjectDescription +
@@ -173,7 +169,7 @@ projects.display = function() {
 
 	}
 };
-projects.display();
+portfolio.display();
 
 //Education
 
@@ -185,10 +181,10 @@ education.display = function() {
 			var FormatedEducationName = HTMLschoolName.replace("%data%", education.schools[item].name);
 			var FormatedEducationDegree = HTMLschoolDegree.replace("%data%", education.schools[item].degree);
 			var FormatedEducationDates = HTMLschoolDates.replace("%data%", education.schools[item].dates);
-			var FormatedEducationLocation = HTMLschoolLocation.replace("%data%", education.schools[item].city);
+			var FormatedEducationLocation = HTMLschoolLocation.replace("%data%", education.schools[item].location);
 			$(".education-entry:last").append(FormatedEducationName + FormatedEducationDegree + FormatedEducationDates + FormatedEducationLocation);
-			for (var major_item in education.schools[item].major) {
-				var FormatedEducationMajor = HTMLschoolMajor.replace("%data%", education.schools[item].major[major_item]);
+			for (var major_item in education.schools[item].majors) {
+				var FormatedEducationMajor = HTMLschoolMajor.replace("%data%", education.schools[item].majors[major_item]);
 				$(".education-entry:last").append(FormatedEducationMajor);
 			}
 		}
